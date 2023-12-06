ba7cdba564f029559c38fa4d313ac812c8a3a828
Chedly Zouche <chedly.zouche@pm.me>
2023-12-06 07:28:06 +0100

    Day 5 (Attempt 2)

--- Day5.cs
+++ Day5.cs
@@ -24,7 +24,7 @@ public class Day5 : IDay<IEnumerable<string>, long>
                 var existingSourceRange = typeRanges.FirstOrDefault(
                     tr => currentType >= tr.Source && currentType <= tr.Source + tr.Count
                 );
-                if (existingSourceRange is not null)
+                if (existingSourceRange != default)
                 {
                     nextType =
                         existingSourceRange.Destination + currentType - existingSourceRange.Source;
@@ -46,9 +46,62 @@ public class Day5 : IDay<IEnumerable<string>, long>
     }
 
     // 🤕!
-    public static long SolvePart2(IEnumerable<string> input) => long.MinValue;
+    public static long SolvePart2(IEnumerable<string> input)
+    {
+        var inputArr = input.ToArray();
 
-    public record MapRule(long Source, long Destination, long Count);
+        List<List<MapEntry>> maps = new();
+        var seedRanges = inputArr[0].Split(": ")[1].Split(' ').Select(long.Parse);
+        var seeds = seedRanges
+            .Chunk(2)
+            .Select(pair => new LongRange(pair[1], pair[0] + pair[1] - 1));
+
+        for (int i = 2; i < inputArr.Length; i++)
+        {
+            var line = inputArr[i];
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
+            if (line.EndsWith(':'))
+            {
+                maps.Add(new List<MapEntry>());
+                continue;
+            }
+
+            var dataStr = line.Split(' ');
+            var destination = long.Parse(dataStr[0]);
+            var source = long.Parse(dataStr[1]);
+            var count = long.Parse(dataStr[2]);
+
+            maps.Last().Add(new MapEntry(source, destination, count));
+        }
+
+        foreach (var item in maps)
+        {
+            Console.WriteLine(string.Join(',', item));
+        }
+        return long.MinValue;
+    }
+
+    public record struct MapEntry(long Source, long Destination, long Length) { }
+
+    public record struct LongRange(long Min, long Max)
+    {
+        public static bool Contains(LongRange a, LongRange b) => a.Min <= b.Min && a.Max >= b.Max;
+
+        public static bool Overlaps(LongRange a, LongRange b, out LongRange intersection)
+        {
+            var check = a.Max >= b.Min && a.Min <= b.Max;
+            var limits = new[] { a.Min, a.Max, b.Min, b.Max }.Order().ToList();
+
+            intersection = check ? new LongRange(limits[1], limits[2]) : default;
+
+            return check;
+        }
+    }
+
+    public record struct MapRule(long Source, long Destination, long Count);
 
     private static Tuple<Dictionary<string, List<MapRule>>, List<string>> BuildMapAndStatesList(
         string[] inputArr
